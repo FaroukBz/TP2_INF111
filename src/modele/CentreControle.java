caa439c15fc71a840ab0cdf7d828b94cefbf0700
FaroukBz <45111965+FaroukBz@users.noreply.github.com>
2019-03-03 15:06:33 -0500

    Add files via upload

---
+++ src/modele/CentreControle.java
@@ -0,0 +1,29 @@
+package modele;
+
+import modele.communication.Message;
+import modele.communication.TransporteurMessage;
+
+public class CentreControle extends TransporteurMessage {
+
+	public CentreControle() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	protected void envoyerMessage(Message msg) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	protected void gestionnaireMessage(Message msg) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
